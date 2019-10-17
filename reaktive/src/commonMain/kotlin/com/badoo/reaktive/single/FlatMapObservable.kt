2095a435a9c0c5d94dcf38bfdf27d4ae9b19a092
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-10-17 17:56:11 +0100

    Optimized Single.flatMapObservable WIP

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/FlatMapObservable.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/single/FlatMapObservable.kt
@@ -1,11 +1,43 @@
 package com.badoo.reaktive.single
 
+import com.badoo.reaktive.base.ErrorCallback
+import com.badoo.reaktive.base.subscribeSafe
+import com.badoo.reaktive.disposable.Disposable
+import com.badoo.reaktive.disposable.DisposableWrapper
 import com.badoo.reaktive.observable.Observable
-import com.badoo.reaktive.observable.flatMap
+import com.badoo.reaktive.observable.ObservableCallbacks
+import com.badoo.reaktive.observable.ObservableObserver
 import com.badoo.reaktive.observable.map
+import com.badoo.reaktive.observable.observableSafe
 
 fun <T, R> Single<T>.flatMapObservable(mapper: (T) -> Observable<R>): Observable<R> =
-    asObservable().flatMap(mapper)
+    observableSafe(::DisposableWrapper) { callbacks, disposableWrapper ->
+        subscribeSafe(
+            object : SingleObserver<T>, ErrorCallback by callbacks {
+                override fun onSubscribe(disposable: Disposable) {
+                    disposableWrapper.set(disposable)
+                }
+
+                override fun onSuccess(value: T) {
+                    val inner =
+                        try {
+                            mapper(value)
+                        } catch (e: Throwable) {
+                            callbacks.onError(e)
+                            return
+                        }
+
+                    inner.subscribeSafe(
+                        object : ObservableObserver<R>, ObservableCallbacks<R> by callbacks {
+                            override fun onSubscribe(disposable: Disposable) {
+                                disposableWrapper.set(disposable)
+                            }
+                        }
+                    )
+                }
+            }
+        )
+    }
 
 fun <T, U, R> Single<T>.flatMapObservable(mapper: (T) -> Observable<U>, resultSelector: (T, U) -> R): Observable<R> =
     flatMapObservable { t ->
