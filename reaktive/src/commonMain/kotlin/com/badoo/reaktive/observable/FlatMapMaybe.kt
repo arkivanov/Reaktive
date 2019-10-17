f5215bf12598c880da68ed12dbc17fedcfa9af29
Arkadii Ivanov <arkadii.ivanov@corp.badoo.com>
2019-10-17 17:55:55 +0100

    Optimized Observable.flatMapMaybe WIP

--- reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/FlatMapMaybe.kt
+++ reaktive/src/commonMain/kotlin/com/badoo/reaktive/observable/FlatMapMaybe.kt
@@ -1,12 +1,53 @@
 package com.badoo.reaktive.observable
 
+import com.badoo.reaktive.base.ErrorCallback
+import com.badoo.reaktive.base.Observer
+import com.badoo.reaktive.base.subscribeSafe
+import com.badoo.reaktive.completable.CompletableCallbacks
+import com.badoo.reaktive.disposable.CompositeDisposable
+import com.badoo.reaktive.disposable.Disposable
 import com.badoo.reaktive.maybe.Maybe
-import com.badoo.reaktive.maybe.asObservable
+import com.badoo.reaktive.maybe.MaybeObserver
 import com.badoo.reaktive.maybe.map
+import com.badoo.reaktive.utils.atomic.AtomicInt
 
 fun <T, R> Observable<T>.flatMapMaybe(mapper: (T) -> Maybe<R>): Observable<R> =
-    flatMap {
-        mapper(it).asObservable()
+    observableSafe(::CompositeDisposable) { callbacks, disposables ->
+        val serializedCallbacks = callbacks.serialize()
+
+        subscribeSafe(
+            object : ObservableObserver<T>, ErrorCallback by serializedCallbacks {
+                private val activeSourceCount = AtomicInt(1)
+
+                private val mappedObserver: MaybeObserver<R> =
+                    object : MaybeObserver<R>, Observer by this, CompletableCallbacks by this {
+                        override fun onSuccess(value: R) {
+                            serializedCallbacks.onNext(value)
+                            onComplete()
+                        }
+                    }
+
+                override fun onSubscribe(disposable: Disposable) {
+                    disposables += disposable
+                }
+
+                override fun onNext(value: T) {
+                    activeSourceCount.addAndGet(1)
+
+                    try {
+                        mapper(value).subscribe(mappedObserver)
+                    } catch (e: Throwable) {
+                        serializedCallbacks.onError(e)
+                    }
+                }
+
+                override fun onComplete() {
+                    if (activeSourceCount.addAndGet(-1) <= 0) {
+                        serializedCallbacks.onComplete()
+                    }
+                }
+            }
+        )
     }
 
 fun <T, U, R> Observable<T>.flatMapMaybe(mapper: (T) -> Maybe<U>, resultSelector: (T, U) -> R): Observable<R> =
